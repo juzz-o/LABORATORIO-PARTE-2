1935eabd85d7262833b634cdae618e794e39cfa2
juzzo <juangiraldo1119593@correo.itm.edu.co>
2025-10-04 19:52:22 -0500

    me falta más de la mitad ),:

--- movie/models.py
+++ movie/models.py
@@ -3,8 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Movie(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.CharField()
-    #image = models.ImageField(upload_to='movies/images/')
-    image = models.ImageField(upload_to='movies/images', null=True, blank=True, default='default.jpg')
+    title = models.CharField(max_length=255)
+    description = models.CharField(blank=True, null=True, max_length=3000)
+    image = models.ImageField(upload_to='movie/images', null=True, blank=True, default='movie/images/default.jpg')
     url = models.URLField(blank=True)
+    genre = models.CharField(blank=True, max_length=250)
+    year = models.IntegerField(blank=True, null=True)
+    def __str__(self):
+        return self.title
