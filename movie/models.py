29cda78677107a96fef8458e6c5e9ff3325731c1
juzzo <juangiraldo1119593@correo.itm.edu.co>
2025-10-10 20:38:46 -0500

    arreglé lo de las imágenes

--- movie/models.py
+++ movie/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(blank=True, null=True, max_length=3000)
-    image = models.ImageField(upload_to='movie/images', null=True, blank=True, default='movie/images/default.jpg')
+    image = models.ImageField(upload_to='movie/images/', null=True, blank=True, default='movie/images/default.jpg')
     url = models.URLField(blank=True)
     genre = models.CharField(blank=True, max_length=250)
     year = models.IntegerField(blank=True, null=True)
